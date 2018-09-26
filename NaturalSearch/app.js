155fd2383697396a1713e269ed68ebbce1f29eff
zero101010 <igotaraujo97@gmail.com>
2018-09-25 23:57:46 -0300

    Add the homepage in the views
    Add the homepage in the node.js like was in django
    
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>

--- NaturalSearch/app.js
+++ NaturalSearch/app.js
@@ -1,16 +1,12 @@
-// Carregue o módulo http para criar um servidor HTTP.
-var http = require('http');
- 
-// Configura nosso servidor HTTP para responder com Olá mundo
-var server = http.createServer(function (request, response) {
-  // Define os parâmetros de cabeçalho de resposta
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  // Envia uma resposta para o cliente com a mensagem Hello World
-  response.end("Hello World\n");
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use('/static',express.static('public'))
+
+app.get('/home', function(req, resp) {
+  resp.sendFile('home.html', {root: path.join(__dirname, 'views')});
+})
+
+app.listen(3000, function() {
+  console.log('Listening at port 3000');
 });
- 
-// Define a porta 8000 onde será executado, o ip padrão é 127.0.0.1 / localhost
-server.listen(3000);
- 
-// Imprime uma mensagem no servidor
-console.log("Server running at http://localhost:3000/");
