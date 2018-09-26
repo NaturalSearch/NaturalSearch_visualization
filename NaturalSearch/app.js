7bb96007d0e8fa6f5b6b2bdae7b08e14da859372
zero101010 <igotaraujo97@gmail.com>
2018-09-26 01:48:20 -0300

    Add D3 in the node.js and use the json of miserables to make a example to use in homepage
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>

--- NaturalSearch/app.js
+++ NaturalSearch/app.js
@@ -7,6 +7,9 @@ app.get('/home', function(req, resp) {
   resp.sendFile('home.html', {root: path.join(__dirname, 'views')});
 })
 
+app.get('/example', function(req, resp) {
+  resp.sendFile('example.html', {root: path.join(__dirname, 'views')});
+})
 app.listen(3000, function() {
   console.log('Listening at port 3000');
 });
