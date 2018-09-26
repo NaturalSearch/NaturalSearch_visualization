600f1698ca9df5072254faf86c4f9a8a5b27d5a6
FabianaRibas <fabianalv.ribas@gmail.com>
2018-09-25 23:15:44 -0300

    Adding search button.
    Co-authored-by: Mikhaelle Bueno <mikhabueno@yahoo.com>

--- NaturalSearch/app.js
+++ NaturalSearch/app.js
@@ -1,16 +1,35 @@
 // Carregue o módulo http para criar um servidor HTTP.
 var http = require('http');
+//file sistem
+var fs = require('fs');
  
 // Configura nosso servidor HTTP para responder com Olá mundo
-var server = http.createServer(function (request, response) {
+function onRequest(request, response) {
   // Define os parâmetros de cabeçalho de resposta
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  // Envia uma resposta para o cliente com a mensagem Hello World
-  response.end("Hello World\n");
-});
- 
-// Define a porta 8000 onde será executado, o ip padrão é 127.0.0.1 / localhost
-server.listen(3000);
+  response.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile('./search.html', null, function(error, data){
+    if(error){
+      response.writeHead(404);
+      response.write('File not found!');
+    } else {
+      response.write(data);
+    }
+    // Envia uma resposta para o cliente com a mensagem Hello World
+    response.end();
+  });
+};
+// Define a porta 3000 onde será executado, o ip padrão é 127.0.0.1 / localhost
+http.createServer(onRequest).listen(3000);
  
 // Imprime uma mensagem no servidor
 console.log("Server running at http://localhost:3000/");
+
+// Open the full screen search box 
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box 
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
