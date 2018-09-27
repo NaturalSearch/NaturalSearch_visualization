028a8543e2504bead5c261348b54a28281c98e61
zero101010 <igotaraujo97@gmail.com>
2018-09-27 18:41:51 -0300

    Add method that translate the json of neo4j to json d3
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>

--- NaturalSearch/app.js
+++ NaturalSearch/app.js
@@ -1,15 +1,22 @@
+// declaração de packges
 var express = require('express');
 var path = require('path');
 var app = express();
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver('bolt://neo4j', neo4j.auth.basic('neo4j','eps'));
+var session = driver.session();
 
+//definição de arquivos html e staticos
 app.use('/static',express.static('public'))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
+//actions
 app.get('/home', function(req, resp) {
   resp.sendFile('home.html', {root: path.join(__dirname, 'views')});
 })
@@ -17,9 +24,6 @@ app.get('/home', function(req, resp) {
 app.get('/example', function(req, resp) {
   resp.sendFile('example.html', {root: path.join(__dirname, 'views')});
 })
-
-var driver = neo4j.driver('bolt://neo4j', neo4j.auth.basic('neo4j','123'));
-var session = driver.session();
 
 app.get('/', function(req, res){
   session
@@ -37,6 +41,38 @@ app.get('/', function(req, res){
 
 var test = require('./public/img/movie.json');
 //console.log(JSON.stringify(test));
+
+
+app.get('/teste', function(req, res){
+  
+  function idIndex(a,id) {
+    for (var i=1;i<a.length;i++) {
+      if (a[i].id == id) return i;}
+    return null;
+  }
+  var nodes=[], links=[];
+  test.results[0].data.forEach(function (row) {
+     row.graph.nodes.forEach(function (n) {
+       if (idIndex(nodes,n.id) == null)
+        nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
+     });
+     links = links.concat( row.graph.relationships.map(function(r) {
+       return {start:idIndex(nodes,r.startNode),end:idIndex(nodes,r.endNode),type:r.type};
+     }));
+  });
+  res.send('porraaa')
+  viz = {nodes:nodes, links:links};
+  
+  //create a new file json to use in d3
+  var fs = require('fs');
+
+  fs.writeFileSync('movied3.json', JSON.stringify(viz));
+
+    console.log("Arquivo salvo");
+}); 
+
+
+
 
 app.listen(3000, function() {
   console.log('Listening at port 3000');
