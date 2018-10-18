abfe92e97d933a2b58f35723430227e06e2eac62
zero101010 <igotaraujo97@gmail.com>
2018-10-18 19:51:17 -0300

    add the foreach to read jsonfile

--- NaturalSearch/routes/proponentes.js
+++ NaturalSearch/routes/proponentes.js
@@ -8,33 +8,48 @@ var proponentes = require('../public/javascripts/proponentes.json');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     list_proponentes = []
+    /*
     //console.log(proponentes.proponentes[0].nome,proponentes.proponentes[0].UF);
     //res.send(proponentes.proponentes[0].nome) ;
-    for (i = 0; i <= 100; i++) {
+    for (i = 0; i <= 99; i++) {
         //console.log(proponentes.proponentes[i].nome);
         nome = proponentes.proponentes[i].nome;
         tipo_pessoa = proponentes.proponentes[i].tipo_pessoa;
         uf = proponentes.proponentes[i].UF;
         municipio = proponentes.proponentes[i].municipio;
         list_proponentes.push(nome, tipo_pessoa, uf, municipio);
+    */
+    proponentes.proponentes.map(
+        item => {
+            nome = item.nome;
+            tipo_pessoa = item.tipo_pessoa;
+            uf = item.UF;
+            municipio = item.municipio;
 
-        session
-            .run('CREATE(n:Proponentes_final{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
-                tipo_pessoa: tipo_pessoa,uf:uf,municipio:municipio,nome: nome
-            })
-            .then(function (result) {
-                console.log(list_proponentes);
-                session.close();
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-        //res.redirect("/");     
-        //res.send(list_proponentes);
-        //console.log(list_proponentes);
-    }
+            console.log(nome);
+            console.log(tipo_pessoa);
+            console.log(uf);
+            console.log(municipio);
 
+            session
+                .run('CREATE(n:Proponentes_salic_parcial{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
+                    tipo_pessoa: tipo_pessoa,
+                    uf: uf,
+                    municipio: municipio,
+                    nome: nome
+                })
+                .then(function (result) {
+                    console.log(list_proponentes);
+                    session.close();
+                })
+                    .catch(function (err) {
+                    console.log(err);
+                    });
 
+        }
+    );
+    res.redirect("/");     
+    
 });
 
 module.exports = router;
