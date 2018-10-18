a4416f6393a41baaf0fa78781d9f9becc3b9c782
zero101010 <igotaraujo97@gmail.com>
2018-10-17 23:22:43 -0300

    Add the new structure of db with the fields into in json
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- NaturalSearch/routes/proponentes.js
+++ NaturalSearch/routes/proponentes.js
@@ -1,30 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var neo4j = require('neo4j-driver').v1;
-var driver = neo4j.driver('bolt://neo4j', neo4j.auth.basic('neo4j','eps'));
+var driver = neo4j.driver('bolt://neo4j', neo4j.auth.basic('neo4j', 'eps'));
 var session = driver.session();
 var proponentes = require('../public/javascripts/proponentes.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     list_proponentes = []
-   //console.log(proponentes.proponentes[0].nome,proponentes.proponentes[0].UF);
-   //res.send(proponentes.proponentes[0].nome) ;
-    for(i=0;i<=100;i++){
+    //console.log(proponentes.proponentes[0].nome,proponentes.proponentes[0].UF);
+    //res.send(proponentes.proponentes[0].nome) ;
+    for (i = 0; i <= 100; i++) {
         //console.log(proponentes.proponentes[i].nome);
         nome = proponentes.proponentes[i].nome;
-        tipo_pessoa =proponentes.proponentes[i].tipo_pessoa;
+        tipo_pessoa = proponentes.proponentes[i].tipo_pessoa;
         uf = proponentes.proponentes[i].UF;
-        municipio =proponentes.proponentes[i].municipio;
-        list_proponentes.push(nome,tipo_pessoa,uf,municipio);
-        
+        municipio = proponentes.proponentes[i].municipio;
+        list_proponentes.push(nome, tipo_pessoa, uf, municipio);
+
         session
-            .run("CREATE(n:Proponentes{nome:{nome}}) RETURN n.nome",{nome:nome})
-            .then(function(result){
-                res.redirect('/');
+            .run('CREATE(n:Proponentes_final{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
+                tipo_pessoa: tipo_pessoa,uf:uf,municipio:municipio,nome: nome
+            })
+            .then(function (result) {
+                console.log(list_proponentes);
                 session.close();
             })
-            .catch(function(err){
+            .catch(function (err) {
                 console.log(err);
             });
         //res.redirect("/");     
