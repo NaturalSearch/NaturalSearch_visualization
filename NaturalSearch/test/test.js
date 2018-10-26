76b31b921936b18c5c5736d44f9fac8d4bdafa5e
zero101010 <igotaraujo97@gmail.com>
2018-10-26 19:46:10 -0300

    Add the test of integration of the return of the urls request
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- NaturalSearch/test/test.js
+++ NaturalSearch/test/test.js
@@ -1,2 +1,44 @@
-var assert = require('assert')
-assert(1==1)
+var request = require('supertest');
+var app = require('../app');
+  
+describe('Get the urls is working', function(){
+    it('test if the index is working', function(done){
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          done()
+        });
+    })
+    it('test if the example is working', function(done){
+        request(app)
+          .get('/example')
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+            done()
+          });
+      })
+      it('test if the load_json is working', function(done){
+        request(app)
+          .get('/load_json')
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+            done()
+          });
+      })
+      it('test if the results is working', function(done){
+        request(app)
+          .get('/result')
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+            done()
+          });
+      })
+    });
+
+
+  
