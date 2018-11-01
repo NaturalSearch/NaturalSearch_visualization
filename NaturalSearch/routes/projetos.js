58dd90e6b0ba01ea78f39b4313f991fc41e7abb2
Mikhaelle <mikhabueno@yahoo.com>
2018-11-01 16:27:07 -0200

    Values without repeat
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- NaturalSearch/routes/projetos.js
+++ NaturalSearch/routes/projetos.js
@@ -20,10 +20,14 @@ router.get('/', function (req, res, next) {
                 nome: json.projetos[i].nome,
             });
             console.log(json.projetos[i].nome);
+        session//1
+            .run('CREATE(n:ProponenteProjeto{proponente: {proponenteParam}}) RETURN n.proponente', {
+                proponenteParam: json.projetos[i].proponente,
+            });
         session
-            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (b:Proponente {proponente: {proponente}})-[r:PROPONENTE]->(a) RETURN a,b', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}), (b:ProponenteProjeto{proponente: {proponenteParam}}) MERGE(b)-[r:PROPONENTE]-(a) RETURN b,a', {
                 nomeParam: json.projetos[i].nome,
-                proponente:json.projetos[i].proponente,
+                proponenteParam: json.projetos[i].proponente,
             });
         session
             .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (c:SegmentoProjeto{segmento:{segmento}})-[r:SEGMENTO]->(a) RETURN a,c', {
@@ -45,81 +49,32 @@ router.get('/', function (req, res, next) {
                 nomeParam: json.projetos[i].nome,
                 ano_projeto: json.projetos[i].ano_projeto,
         });
-        /*session//6
-            .run('CREATE(n:ANOProjeto{ano_projeto:{ano_projeto}}) RETURN n.ano_projeto', {
-                ano_projeto: json.projetos[i].ano_projeto,
-            });
-        session//7
-            .run('CREATE(n:VPROJProjeto{valor_projeto:{valor_projeto}}) RETURN n.valor_projeto', {
-                valor_projeto: json.projetos[i].valor_projeto,
-            });
-        session//8
-            .run('CREATE(n:VSOLProjeto{valor_solicitado:{valor_solicitado}}) RETURN n.valor_solicitado', {
-                valor_solicitado: json.projetos[i].valor_solicitado,
-            });
-        session//9
-            .run('CREATE(n:VCAPProjeto{valor_captado:{valor_captado}}) RETURN n.valor_captado', {
-                valor_captado: json.projetos[i].valor_captado,
-            });  
-        session//10
-            .run('CREATE(n:VAProjeto{valor_aprovado:{valor_aprovado}}) RETURN n.valor_aprovado', {
-                valor_aprovado: json.projetos[i].valor_aprovado,
-            });  
-        session//11
-            .run('CREATE(n:VPROProjeto{valor_proposta:{valor_proposta}}) RETURN n.valor_proposta', {
-                valor_proposta: json.projetos[i].valor_proposta,
-            });  
         session
-            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}), (b:ProponenteProjeto{proponente: {proponenteParam}}) MERGE(b)-[r:PROPONENTE]-(a) RETURN b,a', {
-                nomeParam: json.projetos[i].nome,
-                proponenteParam: json.projetos[i].proponente,
-            });
-        session
-            .run('MATCH(c:SegmentoProjeto{segmento:{segmento}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(c)-[r:SEGMENTO]-(a) RETURN c,a', {
-                nomeParam: json.projetos[i].nome,
-                segmento: json.projetos[i].segmento,
-            });
-        session
-            .run('MATCH(d:AreaProjeto{area:{area}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(d)-[r:AREA]-(a) RETURN d,a', {
-                nomeParam: json.projetos[i].nome,
-                area: json.projetos[i].area,
-            });
-        session
-            .run('MATCH(e:UFProjeto{UF:{UF}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(e)-[r:UF]-(a) RETURN e,a', {
-                nomeParam: json.projetos[i].nome,
-                UF: json.projetos[i].UF,
-            });
-        session
-            .run('MATCH(f:ANOProjeto{ano_projeto:{ano_projeto}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(f)-[r:ANO_PROJETO]-(a) RETURN f,a', {
-                nomeParam: json.projetos[i].nome,
-                ano_projeto: json.projetos[i].ano_projeto,
-            });
-        session
-            .run('MATCH(g:VPROJProjeto{valor_projeto:{valor_projeto}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(g)-[r:VALOR_PROJETO]-(a) RETURN g,a', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (g:VPROJProjeto{valor_projeto:{valor_projeto}})-[r:VALOR_PROJETO]->(a) RETURN a,g', {
                 nomeParam: json.projetos[i].nome,
                 valor_projeto: json.projetos[i].valor_projeto,
-            });
+        });
         session
-            .run('MATCH(h:VSOLProjeto{valor_solicitado:{valor_solicitado}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(h)-[r:VALOR_SOLICITADO]-(a) RETURN h,a', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (h:VSOLProjeto{valor_solicitado:{valor_solicitado}})-[r:VALOR_SOLICITADO]->(a) RETURN a,h', {
                 nomeParam: json.projetos[i].nome,
                 valor_solicitado: json.projetos[i].valor_solicitado,
-            });
+        });
         session
-            .run('MATCH(i:VCAPProjeto{valor_captado:{valor_captado}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(i)-[r:VALOR_CAPTADO]-(a) RETURN i,a', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (i:VCAPProjeto{valor_captado:{valor_captado}})-[r:VALOR_CAPTADO]->(a) RETURN a,i', {
                 nomeParam: json.projetos[i].nome,
                 valor_captado: json.projetos[i].valor_captado,
-            });
+        });
         session
-            .run('MATCH(j:VAProjeto{valor_aprovado:{valor_aprovado}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(j)-[r:VALOR_APROVADO]-(a) RETURN j,a', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (j:VAProjeto{valor_aprovado:{valor_aprovado}})-[r:VALOR_APROVADO]->(a) RETURN a,j', {
                 nomeParam: json.projetos[i].nome,
                 valor_aprovado: json.projetos[i].valor_aprovado,
-            });
+        });
         session
-            .run('MATCH(k:VPROProjeto{valor_proposta:{valor_proposta}}), (a:NomeProjeto{nome: {nomeParam}}) MERGE(k)-[r:VALOR_PROPOSTA]-(a) RETURN k,a', {
+            .run('MATCH(a:NomeProjeto{nome: {nomeParam}}) CREATE UNIQUE (k:VPROProjeto{valor_proposta:{valor_proposta}})-[r:VALOR_APROVADO]->(a) RETURN a,k', {
                 nomeParam: json.projetos[i].nome,
                 valor_proposta: json.projetos[i].valor_proposta,
-            });*/
-        
+        });
+    
     } 
         res.send("teste projeto");
 });
