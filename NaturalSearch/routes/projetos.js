1ca317204ab22826af5f25fd3c027ebd688e7490
marcos-mv <vinicius_kof36@hotmail.com>
2018-11-02 22:05:08 -0300

    Adding methods to get projects and proponents in Neo4j
    Co-authored-by: tacia68 <tacia68@gmail.com>
    Co-authored-by: Mikhaelle Bueno <mikhabueno@yahoo.com>

--- NaturalSearch/routes/projetos.js
+++ NaturalSearch/routes/projetos.js
@@ -10,28 +10,28 @@ var fs = require('fs');
 //Alterar quantidade por count
 
 router.get('/', function (req, res, next) {
-        
-        var projetos ='projetos1.json';   
-        json = JSON.parse(fs.readFileSync('./public/projects/' + projetos, 'utf8'));      
-        for(var i=0;i<json.quantidade;i++){
-            session
-                .run('CREATE(n:TesteProjeto{nome:{nome},proponente:{proponente},segmento:{segmento},area:{area},UF:{UF},ano_projeto:{ano_projeto},valor_projeto:{valor_projeto},valor_solicitado:{valor_solicitado},valor_captado:{valor_captado},valor_aprovado:{valor_aprovado},valor_proposta:{valor_proposta}}) RETURN n.nome', {
-                    nome: json.projetos[i].nome,
-                    proponente:json.projetos[i].proponente,
-                    segmento: json.projetos[i].segmento,
-                    area: json.projetos[i].area,
-                    UF: json.projetos[i].UF,
-                    ano_projeto: json.projetos[i].ano_projeto,
-                    valor_projeto: json.projetos[i].valor_projeto,
-                    valor_solicitado: json.projetos[i].valor_solicitado,
-                    valor_captado: json.projetos[i].valor_captado,
-                    valor_aprovado: json.projetos[i].valor_aprovado,
-                    valor_proposta: json.projetos[i].valor_proposta,
-                });
-                console.log(json.projetos[i].nome);
-        }
-    res.send("teste projeto");
-
+    
+    var projetos ='projetos1.json';
+    json = JSON.parse(fs.readFileSync('./public/projects/' + projetos, 'utf8'));      
+    for(var i=0;i<json.quantidade;i++){
+        session    
+            .run('CREATE(n:TesteProjeto{nome:{nome},proponente:{proponente},segmento:{segmento},area:{area},UF:{UF},ano_projeto:{ano_projeto},valor_projeto:{valor_projeto},valor_solicitado:{valor_solicitado},valor_captado:{valor_captado},valor_aprovado:{valor_aprovado},valor_proposta:{valor_proposta}}) RETURN n.nome', {
+                nome: json.projetos[i].nome,
+                proponente:json.projetos[i].proponente,
+                segmento: json.projetos[i].segmento,
+                area: json.projetos[i].area,
+                UF: json.projetos[i].UF,
+                ano_projeto: json.projetos[i].ano_projeto,
+                valor_projeto: json.projetos[i].valor_projeto,
+                valor_solicitado: json.projetos[i].valor_solicitado,
+                valor_captado: json.projetos[i].valor_captado,
+                valor_aprovado: json.projetos[i].valor_aprovado,
+                valor_proposta: json.projetos[i].valor_proposta,
+            });
+        console.log(json.projetos[i].nome);
+    }
+        res.send("teste projeto");
 });
+
 
 module.exports = router;
--- NaturalSearch/routes/proponentes.js
+++ NaturalSearch/routes/proponentes.js
@@ -7,28 +7,30 @@ var fs = require('fs');
     
     
 /* GET home page. */
-/*
+
 router.get('/', function (req, res, next) {
 
     for (var count = 1; count <=443; count++) {
         
         var proponentes ='proponentes'+count+'.json';   
-        json = JSON.parse(fs.readFileSync('./public/proponents/' + proponentes, 'utf8'));      
+        json = JSON.parse(fs.readFileSync('./public/javascripts/' + proponentes, 'utf8'));      
         for(var i=0;i<json.quantidade;i++){
             session
-                .run('CREATE(n:TesteFinalVerdadeiro{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
+                .run('CREATE(n:Nó_Proponentes{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
                     tipo_pessoa:json.proponentes[i].tipo_pessoa,
                     uf: json.proponentes[i].UF,
                     municipio: json.proponentes[i].municipio,
                     nome: json.proponentes[i].nome
                 });
+            session
+                
                 console.log(json.proponentes[i].nome);
         }
     }
     res.send("teste");
 
 });
-*/
+/*
 
 router.get('/', function (req, res, next) {
         
@@ -36,16 +38,40 @@ router.get('/', function (req, res, next) {
         json = JSON.parse(fs.readFileSync('./public/proponents/' + proponentes, 'utf8'));      
         for(var i=0;i<json.quantidade;i++){
             session
-                .run('CREATE(n:TesteProponente{tipo_pessoa:{tipo_pessoa},uf:{uf},municipio:{municipio},nome:{nome}}) RETURN n.nome', {
-                    tipo_pessoa:json.proponentes[i].tipo_pessoa,
-                    uf: json.proponentes[i].UF,
-                    municipio: json.proponentes[i].municipio,
+                .run('CREATE(n:NomeProponente{nome:{nome}}) RETURN n.nome', {
                     nome: json.proponentes[i].nome
                 });
                 console.log(json.proponentes[i].nome);
+            session
+                .run('CREATE(n:TipoProponente{tipo_pessoa:{tipo_pessoa}}) RETURN n.tipo_pessoa', {
+                    tipo_pessoa:json.proponentes[i].tipo_pessoa,
+                });
+            session
+                .run('CREATE(n:UFProponente{uf:{uf}}) RETURN n.uf', {
+                    uf: json.proponentes[i].UF,
+                });
+            session
+                .run('CREATE(n:MunicipioProponente{municipio:{municipio}}) RETURN n.municipio', {
+                    municipio: json.proponentes[i].municipio,
+                });
+            session
+                .run('MATCH(a:NomeProponente{nome:{nome}}), (b:TipoProponente{tipo_pessoa:{tipo_pessoa}}) MERGE(b)-[r:TIPO_PROPONENTE]-(a) RETURN b,a', {
+                    nome: json.proponentes[i].nome,
+                    tipo_pessoa:json.proponentes[i].tipo_pessoa,
+                });
+            session
+                .run('MATCH(a:NomeProponente{nome:{nome}}), (c:UFProponente{uf:{uf}}) MERGE(c)-[r:UF_PROPONENTE]-(a) RETURN c,a', {
+                    nome: json.proponentes[i].nome,
+                    uf: json.proponentes[i].UF,
+                });
+            session
+                .run('MATCH(a:NomeProponente{nome:{nome}}), (d:MunicipioProponente{municipio:{municipio}}) MERGE(d)-[r:MUNI_PROPONENTE]-(a) RETURN d,a', {
+                    nome: json.proponentes[i].nome,
+                    municipio: json.proponentes[i].municipio,
+                });
         }
     res.send("teste proponente");
-
 });
+**/
 
 module.exports = router;
