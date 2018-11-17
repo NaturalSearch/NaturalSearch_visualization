525cad40ff17b875873718f8b11ded11d213a80d
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-11-16 23:06:09 -0200

    Adding fetch api pages
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- NaturalSearch/routes/projetos.js
+++ NaturalSearch/routes/projetos.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const fetch = require('node-fetch');
 
 
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-    request('http://68.183.107.229:8000/projeto/?limit=100&offset=100',function (error, response, body){
-        console.log('error:', error);
-        console.log('statusCode:', response && response.statusCode); 
-        var json = JSON.parse(body);
-        
-        console.log(json.count);
-        console.log('projetos_id:', json.results);
-    })
+    url = "http://68.183.107.229:8000/projeto/?limit=&offset=" + (page_number*100)
+    //console.log(url);
+    var page_number = 0;
+   
+
+         fetch("http://68.183.107.229:8000/projeto/?limit=&offset=0").then((res)=>{
+                return res.json();
+            }).then((json)=>{
+            total = json.count;
+            reminder = total % 100;
+            total = parseInt(total/100);
+            total = total + 1;
+            for(let page_number = 0; page_number < total; page_number++) {
+                url = "http://68.183.107.229:8000/projeto/?limit=&offset=" + (page_number*100)
+
+                fetch(url).then((res)=>{
+                    return res.json();
+                }).then((json)=>{
+                    console.log(json.results[0].url);
+                    console.log(json.results[1].url);
+                    //console.log(json);
+                    //console.log("outro");
+                })
+            }
+        })
   res.render('projetos', { title: 'Express' });
 });
 
