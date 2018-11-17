6d9e8719cf7ef99b8d6b23740cdf2a7b0cb4383b
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-11-16 23:10:48 -0200

    Adding for loop api pages
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- NaturalSearch/routes/projetos.js
+++ NaturalSearch/routes/projetos.js
@@ -1,39 +1,42 @@
 var express = require('express');
 var router = express.Router();
-const request = require('request');
+var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver('bolt://neo4j', neo4j.auth.basic('neo4j', 'eps'));
+var session = driver.session();
+var fs = require('fs');
 const fetch = require('node-fetch');
-
-
-
+    
 /* GET home page. */
+//Alterar quantidade por count
 
-router.get('/', function(req, res, next) {
-    url = "http://68.183.107.229:8000/projeto/?limit=&offset=" + (page_number*100)
-    //console.log(url);
-    var page_number = 0;
-   
+router.get('/', function (req, res, next) {
+    fetch("http://68.183.107.229:8000/projeto/?limit=&offset=0").then((res)=>{
+        return res.json();
+    }).then((json)=>{
+        total = json.count;
+        reminder = total % 100;
+        total = parseInt(total/100);
+        total = total + 1;
 
-         fetch("http://68.183.107.229:8000/projeto/?limit=&offset=0").then((res)=>{
+        for(let page_number = 0; page_number < total; page_number++) {
+            url = "http://68.183.107.229:8000/projeto/?limit=&offset=" + (page_number*100)
+    
+            fetch(url).then((res)=>{
                 return res.json();
             }).then((json)=>{
-            total = json.count;
-            reminder = total % 100;
-            total = parseInt(total/100);
-            total = total + 1;
-            for(let page_number = 0; page_number < total; page_number++) {
-                url = "http://68.183.107.229:8000/projeto/?limit=&offset=" + (page_number*100)
+                elements_quantity = 100;
+                if (json.next == null) {
+                    elements_quantity = reminder;
+                }
+                for(let json_position = 0; json_position < elements_quantity; json_position++) {
+                    console.log(json.results[json_position].url);
+                }
+            })
+        }
+    })
 
-                fetch(url).then((res)=>{
-                    return res.json();
-                }).then((json)=>{
-                    console.log(json.results[0].url);
-                    console.log(json.results[1].url);
-                    //console.log(json);
-                    //console.log("outro");
-                })
-            }
-        })
-  res.render('projetos', { title: 'Express' });
+        res.send("teste projetos");
 });
+
 
 module.exports = router;
