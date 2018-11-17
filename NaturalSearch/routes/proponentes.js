b070296c8e22a5a50c1b2a540f63361eeb3420d2
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-11-16 23:49:14 -0200

    Adding method loop for, to proponentes.
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- NaturalSearch/routes/proponentes.js
+++ NaturalSearch/routes/proponentes.js
@@ -7,27 +7,31 @@ const fetch = require('node-fetch');
 
 router.get('/', function (req, res, next) {
 
-    fetch("http://68.183.107.229:8000/proponente/?limit=100&offset=0").then((res)=>{
+    fetch("http://68.183.107.229:8000/proponente/?limit=100&offset=").then((res)=>{
         return res.json();
     }).then((json)=>{
-    total = json.count;
-    reminder = total % 100;
-    total = parseInt(total/100);
-    total = total + 1;
-    for(let page_number = 0; page_number < total; page_number++) {
-        url = "http://68.183.107.229:8000/proponente/?limit=100&offset=0" + (page_number*100)
+        total = json.count;
+        reminder = total % 100;
+        total = parseInt(total/100);
+        total = total + 1;
+        for(let page_number = 0; page_number < total; page_number++) {
+            url = "http://68.183.107.229:8000/proponente/?limit=100&offset=" + (page_number*100)
+    
             fetch(url).then((res)=>{
-            return res.json();
-        }).then((json)=>{
-            console.log(json.results[0].url);
-            console.log(json.results[1].url);
-            //console.log(json);
-            //console.log("outro");
-        })
-    }
-})
+                return res.json();
+            }).then((json)=>{
+                elements_quantity = 100;
+                if (json.next == null) {
+                    elements_quantity = reminder;
+                }
+                for(let json_position = 0; json_position < elements_quantity; json_position++) {
+                    console.log(json.results[json_position].url);
+                }
+            })
+        }
+    })
 
-    res.send("vai funcionar caramba");
+    res.send("Carregando todos os proponentes!");
 });
 
 
